674fd6585f48b14416f29b61cede67553a344bab
Pe4eNPe4eNkI <av59ru@yandex.ru>
2024-04-04 22:41:59 +0500

    hw 2 fix

--- src/test/java/edu/phystech/hw2/analyzer/KeywordAnalyzer.java
+++ src/test/java/edu/phystech/hw2/analyzer/KeywordAnalyzer.java
@@ -3,22 +3,22 @@ package edu.phystech.hw2.analyzer;
 import java.util.List;
 
 public abstract class KeywordAnalyzer implements TextAnalyzer {
-    private final List<String> valuesToBan_;
+    private final List<String> valuesToBan;
 
-    private final Label badCaseLabel_;
+    private final Label badCaseLabel;
 
     public KeywordAnalyzer(List<String> valuesToBan, Label label) {
-        this.valuesToBan_ = valuesToBan;
-        badCaseLabel_ = label;
+        this.valuesToBan = valuesToBan;
+        badCaseLabel = label;
     }
 
 
     public Label processText(String text) {
         List<String> splitText =  List.of(text.split(" "));
         for (var elem : splitText) {
-            for (var banValue : valuesToBan_) {
+            for (var banValue : valuesToBan) {
                 if (banValue.equals(elem))
-                    return badCaseLabel_;
+                    return badCaseLabel;
             }
         }
         return Label.OK;
